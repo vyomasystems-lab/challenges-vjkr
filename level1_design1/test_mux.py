4c3e8949a0a4151d73bd4b6b6834d4ae416a84f9
Vijaykumar <hi.vjkr@gmail.com>
2022-07-28 06:30:25 +0000

    directtestruns

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -15,25 +15,4 @@ async def test_mux(dut):
     
     # await Timer(2, units='ns') <======== SHOULD NEVER BE COMMENTED, RESULTS IN VALUE ERROR
     await Timer(2, units='ns')
-    assert dut.out.value == 7, "I failed my Mux, expected vlaue = {EXP}" .format(EXP=int(dut.out.value))
-    
-    # assert dut.sum.value == A+B, "Adder result is incorrect: {A} + {B} != {SUM}, expected value={EXP}".format(
-    #         A=int(dut.a.value), B=int(dut.b.value), SUM=int(dut.sum.value), EXP=A+B)
-async def test_mux(dut):
-    """Test for mux2"""
-
-    cocotb.log.info('##### CTB: Develop your test here ########')
-   
-    # input driving
-    dut.sel.value = 5
-    dut.inp5.value = 3
-    
-    # await Timer(2, units='ns') <======== SHOULD NEVER BE COMMENTED, RESULTS IN VALUE ERROR
-    await Timer(2, units='ns')
-    assert dut.out.value == 1, "Mux failed, expected vlaue = {EXP}" .format(EXP=int(dut.out.value))
-
-    # # input driving
-    # dut.sel.value = 10
-    # dut.inp10.value = 2
-    # await Timer(2, units='ns')
-    # assert dut.out.value == 2, "Mux failed, expected vlaue = {EXP}" .format(EXP=int(dut.out.value))
+    assert dut.out.value == 3, "I failed my Mux, expected vlaue = {EXP}" .format(EXP=int(dut.out.value))
